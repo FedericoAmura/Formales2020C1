b761d46dd95b3c624737986e25a8536180072bba
Federico Amura <federicoamura@gmail.com>
2020-07-18 22:42:31 -0300

    controlar-aridad tests que faltaron en el commit anterior

--- test/tlclisp/core_test.clj
+++ test/tlclisp/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [tlclisp.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; controlar-aridad
+(deftest controlar-aridad-few-arguments
+  (testing "Should return too few arguments if that happens"
+    (is (= '(*error* too-few-args) (controlar-aridad '(a b c) 4)))))
+
+(deftest controlar-aridad-correct
+  (testing "Should return argument count when correct"
+    (is (= 4 (controlar-aridad '(a b c d) 4)))))
+
+(deftest controlar-aridad-many-arguments
+  (testing "Should return too many arguments if that happens"
+    (is (= '(*error* too-many-args) (controlar-aridad '(a b c d e) 4)))))
